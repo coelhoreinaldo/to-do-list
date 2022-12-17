47dbc6b819a353281d71f239c189f8303cbf90c0
coelhoreinaldo <scoelhor7@gmail.com>
2022-12-16 21:03:50 -0300

    requisito 7 kkkkk to cansado

--- script.js
+++ script.js
@@ -2,21 +2,47 @@ const createTaskBtn = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 
+const taskItemsLocalStorage = () => {
+  localStorage.setItem('taskItem', taskList.innerHTML);
+};
+
 const addTask = () => {
   createTaskBtn.addEventListener('click', () => {
     const inputValue = input.value;
 
     const createTask = document.createElement('li');
+    createTask.className = 'task-item';
     createTask.innerHTML = inputValue;
     taskList.appendChild(createTask);
 
+    taskItemsLocalStorage();
     input.value = '';
   });
 };
 
 addTask();
 
-// A página deve possuir um elemento do tipo button;
-// O elemento button deve possuir o ID criar-tarefa;
-// Ao digitar o texto minha primeira tarefa e clicar no botão criar-tarefa, o texto digitado deve aparecer na lista e desaparecer do campo de input;
-// A adição de elementos na lista será feita algumas vezes, portanto todos os itens criados devem permanecer na lista na medida em que novos itens são adicionados.
+const clickTask = () => {
+  const taskItems = document.querySelectorAll('.task-item');
+
+  for (let i = 0; i < taskItems.length; i += 1) {
+    taskItems[i].addEventListener('click', () => {
+      taskItems[i].style.backgroundColor = 'rgb(128, 128, 128)';
+    });
+  }
+  console.log(taskItems);
+};
+
+window.onload = () => {
+  const getTaskItem = localStorage.getItem('taskItem');
+  taskList.innerHTML = getTaskItem;
+  clickTask();
+};
+
+// Ao clicar em um item da lista, este deve adquirir a cor adicionada à folha de estilo com o padrão: `background-color: nome-da-cor`, não sendo permitido qualquer outro padrão de nomenclatura de cores.
+
+// O que será testado:
+
+// A página ao ser carregada deve possuir os itens da lista sem o estilo CSS background-color: gray;
+
+// Os itens da lista ao serem clicados devem passar a ter o estilo CSS background-color: gray.
