ff2bb6bb8de27dfc837398db5a2a80e3fd78a0e4
coelhoreinaldo <scoelhor7@gmail.com>
2023-07-06 17:36:43 -0300

    ✨ feat: add new task

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const Main = styled.main`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const Header = styled.header`
+  position:fixed;
+  top: 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+interface Task {
+  id: number;
+  name: string;
 }
 
-export default App
+function App() {
+  const [task, setTask] = useState<string>('');
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTask(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newId = tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1;
+    const newTask = {
+      id: newId,
+      name: task,
+    };
+    setTasks([...tasks, newTask]);
+    setTask('');
+  };
+
+  return (
+    <Main>
+      <Header>
+        <h1>To do list</h1>
+        <h2>Clique duas vezes em um item para marcá-lo como completo.</h2>
+      </Header>
+      <form onSubmit={ handleSubmit }>
+        <label>
+          <input
+            type="text"
+            placeholder="Digite uma tarefa..."
+            onChange={ handleChange }
+            value={ task }
+          />
+        </label>
+        <ol>
+          {tasks.map((item) => <li key={ item.id }>{item.name}</li>)}
+        </ol>
+        <button type="submit">Adicionar</button>
+      </form>
+
+    </Main>
+  );
+}
+
+export default App;
