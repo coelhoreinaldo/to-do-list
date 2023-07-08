63644c2f61423ed62ad9096f9c672606654d2f04
coelhoreinaldo <scoelhor7@gmail.com>
2023-07-07 23:47:17 -0300

    📚 docs: add github

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,9 @@ const HeaderStyled = styled.header`
   position:fixed;
   top: 0;
   display: flex;
-  flex-direction: column;
+  justify-content: center;
+  gap: 8px;
+  align-items: flex-end;
   background: transparent;
   color: white;
   width: 100%;
@@ -14,10 +16,24 @@ const HeaderStyled = styled.header`
 const H1 = styled.h1`
 `;
 
+const A = styled.a`
+  color: white;
+  font-size: 1.7rem;
+  text-decoration: none;
+  transition: all;
+  transition-duration: 150ms;
+
+&:hover {
+  color: aquamarine;
+}
+`;
 export default function Header() {
   return (
     <HeaderStyled>
       <H1>To do list</H1>
+      <A href="https://github.com/coelhoreinaldo/to-do-list" target="_blank" rel="noreferrer">
+        <i className="ri-github-fill" />
+      </A>
     </HeaderStyled>
   );
 }
